83a6596ad86607c8abb83081d32197dfda7038d5
Spencer Sansone <spencersemail101@gmail.com>
2018-09-03 19:13:54 +0000

    improve add_event

--- main/views.py
+++ main/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import Q
+
+def getTodayDateTime():
+    return datetime.now()
+    
+def getWeekdayArray():
+    return ["monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday"]
 
 def home(request):
     return render(request, 'main/home.html')
@@ -51,38 +64,62 @@ def add_event(request):
         print(e_d)
         e_t = request.POST.get('end_time')
         print(e_t)
+        no_repeat = True if request.POST.get('no_repeat') == "on" else False
+        print("no rep?")
+        print(no_repeat)
+        yes_repeat = True if request.POST.get('yes_repeat') == "on" else False
+        print("yes rep?")
+        print(yes_repeat)
         
-        
-        
-        # 18:37 from mobile
-        # 2:00am from desktop
-        
-        
-        
+        r_e = False
+        sun = False
+        mon = False
+        tue = False
+        wed = False
+        thu = False
+        fri = False
+        sat = False
+        if yes_repeat:
+            r_e = True
+            sun = True if request.POST.get('sunday') == "on" else False
+            mon = True if request.POST.get('monday') == "on" else False
+            tue = True if request.POST.get('tuesday') == "on" else False
+            wed = True if request.POST.get('wednesday') == "on" else False
+            thu = True if request.POST.get('thursday') == "on" else False
+            fri = True if request.POST.get('friday') == "on" else False
+            sat = True if request.POST.get('saturday') == "on" else False
 
-        sanitize_time(s_t)
-        sanitize_time(e_t)
+        
+        
         
         Event.objects.create(
             title = t,
             description = d,
             location = l,
             start_date = s_d,
-            start_time = s_t,
+            start_time = sanitize_time(s_t),
             end_date = e_d,
-            end_time = e_t,
-            repeat_event = False,
-            sunday = False,
-            monday = False,
-            tuesday = False,
-            wednesday = False,
-            thursday = False,
-            friday = False,
-            saturday = False)
+            end_time = sanitize_time(e_t),
+            repeat_event = r_e,
+            sunday = sun,
+            monday = mon,
+            tuesday = tue,
+            wednesday = wed,
+            thursday = thu,
+            friday = fri,
+            saturday = sat)
         
         # return render(request, 'main/event_list.html')
         return HttpResponseRedirect(reverse('main:event_list'))
         
     return render(request, 'main/add_event.html')
+    
+# def today_agenda(request):
+#     today = getTodayDateTime()
+#     today_weekday = getWeekdayArray()[today.weekday()]
+#     query1 = Q(**{today_weekday: True})
+#     query2 = Q(date=today)
+#     today_events = Event.objects.filter( query1 | query2 )
+    
 
 # Create your views here.
